89ec47bb7d37922181e8a95e1b25f1a96b13ceb8
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-01-08 02:40:10 -0300

    feat: responsive login/registration pages

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -92,6 +92,10 @@ const LoginPageScreen = styled.div`
 	align-items: center;
 	justify-content: space-between;
 	background-color: #151515;
+	@media (max-width: 1170px) {
+		width: 100vw;
+		flex-direction: column;
+	}
 `;
 
 const LoginPageTitle = styled.div`
@@ -102,6 +106,11 @@ const LoginPageTitle = styled.div`
 	align-items: center;
 	justify-content: flex-start;
 	padding: 144px;
+	@media (max-width: 1170px) {
+		height: auto;
+		justify-content: center;
+		padding: 0px;
+	}
 `;
 
 const TitleContainer = styled.div`
@@ -121,6 +130,20 @@ const TitleContainer = styled.div`
 		line-height: 64px;
 		color: #ffffff;
 	}
+	@media (max-width: 1170px) {
+		width: 237px;
+		padding: 30px 0px;
+		h1 {
+			font-size: 76px;
+			line-height: 84px;
+			text-align: center;
+		}
+		p {
+			font-size: 23px;
+			line-height: 34px;
+			text-align: center;
+		}
+	}
 `;
 
 const LoginPageContainer = styled.div`
@@ -130,6 +153,11 @@ const LoginPageContainer = styled.div`
 	align-items: center;
 	justify-content: center;
 	background-color: #333333;
+	@media (max-width: 1170px) {
+		height: auto;
+		flex-grow: 1;
+		align-items: flex-start;
+	}
 `;
 
 const FormContainer = styled.div`
@@ -148,6 +176,15 @@ const FormContainer = styled.div`
 		text-decoration: underline;
 		cursor: pointer;
 	}
+	@media (max-width: 1170px) {
+		width: 330px;
+		margin-top: 40px;
+		gap: 15px;
+		> button {
+			font-size: 17px;
+			line-height: 20px;
+		}
+	}
 `;
 
 const Form = styled.form`
@@ -165,6 +202,13 @@ const Form = styled.form`
 		font-weight: 700;
 		font-size: 27px;
 		line-height: 40px;
+	}
+	@media (max-width: 1170px) {
+		gap: 10px;
+		input {
+			font-size: 22px;
+			line-height: 33px;
+		}
 	}
 `;
 
@@ -185,4 +229,9 @@ const ButtonItem = styled.button`
 	color: #ffffff;
 	background-color: #1877f2;
 	cursor: pointer;
+	@media (max-width: 1170px) {
+		height: 55px;
+		font-size: 22px;
+		line-height: 33px;
+	}
 `;
